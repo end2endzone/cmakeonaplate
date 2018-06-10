f2fed3c7cc51a40ad2d69c194d77defabc68d04f
Antoine <end2endzone@gmail.com>
2018-06-10 08:58:08 -0400

    Updated todo.txt Created ci/appveyor's scripts: before_build.bat and install_googletest.bat Reordered sections of the library's CMakeLists.txt Inserted code in library's CMakeLists.txt to automatically set build type to Release if not specified.

---
+++ library/ci/appveyor/install_googletest.bat
@@ -0,0 +1,42 @@
+@echo off
+
+:: Allow the script to be launch locally or from AppVeyor
+cd /d %~dp0
+cd ..\..\..
+set APPVEYOR_BUILD_FOLDER_LOCAL=%cd%
+
+:: Detect if script is launch manually or from AppVeyor.
+if "%APPVEYOR_BUILD_FOLDER%"=="" (
+  set APPVEYOR_BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER_LOCAL%
+  echo Building locally: APPVEYOR_BUILD_FOLDER set to %APPVEYOR_BUILD_FOLDER_LOCAL%
+)
+set APPVEYOR_BUILD_FOLDER_LOCAL=
+
+echo ============================================================================
+echo Cloning googletest into %APPVEYOR_BUILD_FOLDER%\submodules\googletest
+echo ============================================================================
+mkdir %APPVEYOR_BUILD_FOLDER%\submodules >NUL 2>NUL
+cd %APPVEYOR_BUILD_FOLDER%\submodules
+git clone "https://github.com/google/googletest.git"
+cd googletest
+echo.
+
+echo Checking out version 1.8.0...
+git checkout release-1.8.0
+echo.
+
+echo ============================================================================
+echo Compiling...
+echo ============================================================================
+mkdir build >NUL 2>NUL
+cd build
+set GTEST_ROOT=%APPVEYOR_BUILD_FOLDER%\installs
+cmake -DCMAKE_INSTALL_PREFIX=%GTEST_ROOT% -Dgtest_force_shared_crt=ON ..
+cmake --build .
+echo.
+
+echo ============================================================================
+echo Installing into %GTEST_ROOT%
+echo ============================================================================
+cmake --build . --target INSTALL
+echo.
