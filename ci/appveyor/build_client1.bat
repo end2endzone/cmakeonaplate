dff19c4428e8b7a29ab4c8b35265c1a2d600e52c
Antoine <end2endzone@gmail.com>
2018-07-22 12:21:44 -0400

    * Updated todo.txt * Updated all build script to exit if AppVeyor's environment is not found. * Created emulate_appveyor.bat script which locally creates a fake AppVeyor's environment variable.

--- ci/appveyor/build_client1.bat
+++ ci/appveyor/build_client1.bat
@@ -1,17 +1,10 @@
 @echo off
 
-:: Allow the script to be launch locally or from AppVeyor
-cd /d %~dp0
-cd ..\..
-set APPVEYOR_BUILD_FOLDER_LOCAL=%cd%
-
-:: Detect if script is launch manually or from AppVeyor.
+:: Validate appveyor's environment
 if "%APPVEYOR_BUILD_FOLDER%"=="" (
-  set APPVEYOR_BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER_LOCAL%
-  echo Building locally: APPVEYOR_BUILD_FOLDER set to %APPVEYOR_BUILD_FOLDER_LOCAL%
+  echo Please define 'APPVEYOR_BUILD_FOLDER' environment variable.
+  exit /B 1
 )
-set APPVEYOR_BUILD_FOLDER_LOCAL=
-
 
 ::Copy all installed packages to the same directory
 set MERGED_INSTALL_DIR=%APPVEYOR_BUILD_FOLDER%\install_merge
