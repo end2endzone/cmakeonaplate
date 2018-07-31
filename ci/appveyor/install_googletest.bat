14a62e2833f03de3189eb56bbe0daaa7cf900a55
Antoine <end2endzone@gmail.com>
2018-07-30 21:19:33 -0400

    Fixed install_googletest.bat to quit if %APPVEYOR_BUILD_FOLDER% is not defined.

--- ci/appveyor/install_googletest.bat
+++ ci/appveyor/install_googletest.bat
@@ -1,16 +1,10 @@
 @echo off
 
-:: Allow the script to be launch locally or from AppVeyor
-cd /d %~dp0
-cd ..\..
-set APPVEYOR_BUILD_FOLDER_LOCAL=%cd%
-
-:: Detect if script is launch manually or from AppVeyor.
+:: Validate appveyor's environment
 if "%APPVEYOR_BUILD_FOLDER%"=="" (
-  set APPVEYOR_BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER_LOCAL%
-  echo Building locally: APPVEYOR_BUILD_FOLDER set to %APPVEYOR_BUILD_FOLDER_LOCAL%
+  echo Please define 'APPVEYOR_BUILD_FOLDER' environment variable.
+  exit /B 1
 )
-set APPVEYOR_BUILD_FOLDER_LOCAL=
 
 echo ============================================================================
 echo Cloning googletest into %APPVEYOR_BUILD_FOLDER%\third_parties\googletest
